3098368316eaca30325d075e20d2014ac21aaad0
susuflex <mbarkon@susuflex.com>
2026-03-30 01:49:48 -0500

    Auto-create DB directory on startup

--- db.js
+++ db.js
@@ -4,12 +4,15 @@
 
 const Database = require('better-sqlite3');
 const path = require('path');
+const fs = require('fs');
 
 const DB_PATH = process.env.DB_PATH || path.join(__dirname, 'goldpot.db');
 
 let db;
 
 function init() {
+  const dir = path.dirname(DB_PATH);
+  if (!fs.existsSync(dir)) { fs.mkdirSync(dir, { recursive: true }); }
   db = new Database(DB_PATH);
   db.pragma('journal_mode = WAL');
   db.pragma('foreign_keys = ON');
